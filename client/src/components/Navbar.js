7c1e67e540efc730ea7112eb77d300fb1756c00b
Fatemeh Eshghi <157709672+Fatemeh-Eshghi@users.noreply.github.com>
2024-12-05 12:27:36 +0200

    change login adreess

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -3,22 +3,25 @@ import React from "react";
 function Navbar() {
   const user = JSON.parse(localStorage.getItem("currentUser"));
 
-  function logout(){
- localStorage.removeItem('currentUser')
- window.location.href='./login'
+  function logout() {
+    localStorage.removeItem("currentUser");
+    window.location.href = "/login";
   }
   function capitalizeFirstWord(name) {
     if (!name) return "";
     const words = name.split(" ");
-    words[0] = words[0].charAt(0).toUpperCase() + words[0].slice(1).toLowerCase();
+    words[0] =
+      words[0].charAt(0).toUpperCase() + words[0].slice(1).toLowerCase();
     return words.join(" ");
   }
   return (
     <div>
       <nav className="navbar navbar-expand-lg ">
-        <b><a className="navbar-brand" href="/" style={{ color: "#5d4829" }}>
-          Massage Reservation
-        </a></b>
+        <b>
+          <a className="navbar-brand" href="/" style={{ color: "#5d4829" }}>
+            Massage Reservation
+          </a>
+        </b>
         <button
           className="navbar-toggler"
           type="button"
@@ -27,33 +30,40 @@ function Navbar() {
           aria-controls="navbarNav"
           aria-expanded="false"
           aria-label="Toggle navigation"
-        
         >
-          <span className="navbar-toggler-icon" ><i class="fa fa-bars" style={{color:'white', backgroundColor: 'black'}}></i></span>
+          <span className="navbar-toggler-icon">
+            <i
+              class="fa fa-bars"
+              style={{ color: "white", backgroundColor: "black" }}
+            ></i>
+          </span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav mr-3">
             {user ? (
               <>
-                <div className="dropdown dpnav"   >
+                <div className="dropdown dpnav">
                   <button
                     class="btn btn-secondary dropdown-toggle"
                     type="button"
                     data-toggle="dropdown"
                     aria-expanded="false"
-                    style={{ backgroundColor: "black", color: "white", borderColor: "black" }}
+                    style={{
+                      backgroundColor: "black",
+                      color: "white",
+                      borderColor: "black",
+                    }}
                   >
-                  <i className="fa fa-user"></i> {capitalizeFirstWord(user.name)}
-
+                    <i className="fa fa-user"></i>{" "}
+                    {capitalizeFirstWord(user.name)}
                   </button>
                   <div class="dropdown-menu">
                     <a class="dropdown-item" href="/profile">
-                     Profile
+                      Profile
                     </a>
                     <a class="dropdown-item" href="#" onClick={logout}>
                       Logout
                     </a>
-                    
                   </div>
                 </div>
               </>
